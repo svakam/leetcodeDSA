45048fd5f86ec7d1ef6bf1d10070cf50e553069b
svakam <vikramakam@hotmail.com>
2020-05-12 20:27:39 -0700

    LC code added

--- src/main/java/leetcodeDSA/LongestSubStrPalindrome.java
+++ src/main/java/leetcodeDSA/LongestSubStrPalindrome.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 
 public class LongestSubStrPalindrome {
-    // first attempt: time limit exceeded (runtime: 634 ms)
+    // first attempt: time limit exceeded (runtime: 634 ms) and probably memory too
     public boolean isPalindromeFirstAttempt(String s, int start, int end) {
         while (start != end) {
             if (s.charAt(start) == s.charAt(end) && (end - start > 0)) {
@@ -60,5 +60,29 @@ public class LongestSubStrPalindrome {
         } else {
             return s;
         }
+    }
+
+    // leetcode solution
+    public int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R--;
+        }
+        return R - L - 1;
+    }
+    public String longestPalinLC(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
     }
 }
