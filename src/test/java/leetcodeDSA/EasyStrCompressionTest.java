6baa262b2c76b4c100fae745eb27401639a00f26
svakam <vikramakam@hotmail.com>
2020-07-10 01:23:14 -0700

    testing in prog

--- src/test/java/leetcodeDSA/EasyStrCompressionTest.java
+++ src/test/java/leetcodeDSA/EasyStrCompressionTest.java
@@ -1,11 +1,13 @@
 package leetcodeDSA;
 
 import org.junit.Test;
-import leetcodeDSA.EasyStrCompression;
+import static org.junit.Assert.*;
 
 public class EasyStrCompressionTest {
     @Test
     public void test() {
-        
+        EasyStrCompression e = new EasyStrCompression();
+        assertEquals("abc", e.strCompression("abc"));
+        assertEquals("a6b7c7", e.strCompression("aaaaaabbbbbbbccccccc"));
     }
 }
